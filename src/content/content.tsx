f85fb1bcf6c5de381e27670f7e46eda5341905d0
Ivan Vukman <ivan.vukman@gmail.com>
2020-07-01 21:40:02 +0200

    make use of mutation observer to get #end element

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -2,9 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/app';
 
-// setTimeout(() => {
+const observer = new MutationObserver(() => {
   const endElement = document.querySelector("#end");
-  const appContainer = document.createElement("div");
-  endElement.insertBefore(appContainer, endElement.firstChild);
-  ReactDOM.render(<App />, appContainer);
-// }, 1000);
+  if(endElement) {
+    const appContainer = document.createElement("div");
+    endElement.insertBefore(appContainer, endElement.firstChild);
+    ReactDOM.render(<App />, appContainer);
+    observer.disconnect();
+  }
+});
+
+observer.observe(document, {attributes: true, childList: true, subtree: true});
